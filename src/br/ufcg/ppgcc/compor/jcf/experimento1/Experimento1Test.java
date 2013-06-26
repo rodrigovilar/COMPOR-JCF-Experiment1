c1451b6ada29909dcd176d78b0b8cf6028f1b67a
Delano <Delano@Delano-PC>
2013-06-26 19:02:44 -0300

    Implementados alguns testes.

--- src/br/ufcg/ppgcc/compor/jcf/experimento1/Experimento1Test.java
+++ src/br/ufcg/ppgcc/compor/jcf/experimento1/Experimento1Test.java
@@ -44,36 +44,96 @@ public class Experimento1Test {
 
 	@Test
 	public void duasFontesPagadorasEmUmaDeclaracao() {
+		FontePagadora fonte1 = criarFontePagadoraPadrao1();
+		FontePagadora fonte2 = criarFontePagadoraPadrao2();
 		
+		fachada.criarNovaDeclaracao(criarDeclaracaoPadrao());
+		fachada.criarFontePagadora(fonte1);
+		fachada.criarFontePagadora(fonte2);
+		List<FontePagadora> fontes = fachada.listarFontesPagadoras();
+
+		assertEquals(2, fontes.size());
+		assertEquals(fonte1, fontes.get(0));
+		assertEquals(fonte2, fontes.get(0));
 	}
 
 	@Test
 	public void duasDeclaracoes() {
+		Declaracao declaracao1 = criarDeclaracaoPadrao();
+		Declaracao declaracao2 = criarDeclaracaoMinima();
 		
+		fachada.criarNovaDeclaracao(declaracao1);
+		fachada.criarNovaDeclaracao(declaracao2);
+		List<Declaracao> declaracoes = fachada.listarDeclaracoes();
+		
+		assertEquals(2, declaracoes.size());
+		assertEquals(declaracao1, declaracoes.get(0));
+		assertEquals(declaracao2, declaracoes.get(1));
 	}
 
 	@Test
 	public void duasFontesPagadorasUmaEmCadaDeclaracao() {
+		Declaracao declaracao1 = criarDeclaracaoPadrao();
+		Declaracao declaracao2 = criarDeclaracaoMinima();
 		
+		fachada.criarNovaDeclaracao(declaracao1);
+		fachada.criarNovaDeclaracao(declaracao2);
+		List<Declaracao> declaracoes = fachada.listarDeclaracoes();
+		
+		assertEquals(2, declaracoes.size());
+		assertEquals(declaracao1, declaracoes.get(0));
+		assertEquals(declaracao2, declaracoes.get(1));
+
 	}
 
 	@Test
 	public void calculoImpostoIsento() {
-		//9000
-
-		//assert que o a pagar é 0
-		//assert que o total é 0
-		//assert que o total pago é 0
-	}
-
-	@Test
-	public void calculoImpostoFaixa1() {
-		//22500
+		//15000
 		//pago 100
 		
-		//assert que o a pagar é 114,10
-		//assert que o total é 214,10
-		//assert que o total pago é 100
+		//assert que o total final  -100,00
+		//assert que o total a pagar 0
+		//assert que o total pago 100
+	}
+	
+	@Test
+	public void calculoImpostoFaixa2() {
+		//22500
+		//pago 150
+		
+		//assert que o total final 64,10
+		//assert que o total a pagar 214,10
+		//assert que o total pago 150
+	}
+	
+	@Test
+	public void calculoImpostoFaixa3() {
+		//35000
+		//pago 200
+		
+		//assert que o total final  1368,45
+		//assert que o total a pagar 1568,45
+		//assert que o total pago 200
+	}
+	
+	@Test
+	public void calculoImpostoFaixa4() {
+		//45000
+		//pago 250
+		
+		//assert que o total final  3249,21
+		//assert que o total a pagar 3499,21
+		//assert que o total pago 250
+	}
+	
+	@Test
+	public void calculoImpostoFaixa5() {
+		//55000
+		//pago 300
+		
+		//assert que o total final  5746,62
+		//assert que o total a pagar 6046,62
+		//assert que o total pago 300
 	}
 
 	
@@ -85,6 +145,17 @@ public class Experimento1Test {
 		fonte.setContribuicaoPrevidenciariaOficial(5000);
 		fonte.setImpostoRetidoNaFonte(8000);
 		fonte.setDecimoTerceiroSalario(5000);
+		return fonte;
+	}
+	
+	private FontePagadora criarFontePagadoraPadrao2() {
+		FontePagadora fonte = new FontePagadora();
+		fonte.setNome("UFPB");
+		fonte.setCpfCnpj("000.000.000/0000-00");
+		fonte.setRendimentoRecebidos(20000);
+		fonte.setContribuicaoPrevidenciariaOficial(2000);
+		fonte.setImpostoRetidoNaFonte(900);
+		fonte.setDecimoTerceiroSalario(2000);
 		return fonte;
 	}
 
